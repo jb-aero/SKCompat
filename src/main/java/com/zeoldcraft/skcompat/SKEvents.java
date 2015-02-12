6916f0b6f6f172c20aa0c6b324f1a2b6f84bd66d
jb_aero <jb_aero@zeoldcraft.com>
2015-02-12 07:45:35 -0500

    Allow WorldGuard to cancel aliases if they are not allowed in a region.

---
+++ src/main/java/com/zeoldcraft/skcompat/SKEvents.java
@@ -0,0 +1,16 @@
+package com.zeoldcraft.skcompat;
+
+import com.laytonsmith.abstraction.events.MCPlayerCommandEvent;
+import com.laytonsmith.annotations.event;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.bukkit.listener.WorldGuardPlayerListener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class SKEvents {
+
+	@event
+	public void onCommand(MCPlayerCommandEvent event) {
+		WorldGuardPlayerListener wgpl = new WorldGuardPlayerListener(WorldGuardPlugin.inst());
+		wgpl.onPlayerCommandPreprocess((PlayerCommandPreprocessEvent) event._GetObject());
+	}
+}
