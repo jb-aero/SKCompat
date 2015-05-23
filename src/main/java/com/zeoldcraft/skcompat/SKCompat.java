963d4ea708421a8aac6d68e1faa88788b0b5ca08
PseudoKnight <michael.jt.smith@gmail.com>
2015-05-23 02:29:30 -0700

    Bump version to 1.1.8

--- src/main/java/com/zeoldcraft/skcompat/SKCompat.java
+++ src/main/java/com/zeoldcraft/skcompat/SKCompat.java
@@ -29,7 +29,7 @@ public class SKCompat extends AbstractExtension {
 
 	@Override
 	public Version getVersion() {
-		return new SimpleVersion(1, 1, 7);
+		return new SimpleVersion(1, 1, 8);
 	}
 
 	public static MCCommandSender myGetPlayer(Construct arg, Target t) {
